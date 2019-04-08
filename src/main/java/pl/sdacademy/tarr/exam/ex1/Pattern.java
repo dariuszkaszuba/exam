ef00187f6930a6ba364c97153df762d36f872bb3
Dariusz Kaszuba <dariusz.kaszuba11@gmail.com>
2019-04-08 09:39:50 +0200

    start

--- src/main/java/pl/sdacademy/tarr/exam/ex1/Pattern.java
+++ src/main/java/pl/sdacademy/tarr/exam/ex1/Pattern.java
@@ -5,7 +5,22 @@ public class Pattern {
 	public String prepareSquareWithDiagonals(char sign, int size) {
 		String pattern = "";
 
-		// TODO: implementation
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (j == size - 1) {
+					pattern += sign;
+				} else if (i == 0 || i == (size - 1)) {
+					pattern += sign + " ";
+
+				} else if (j == 0 || j == (size - 1) || i == j || i == (size - 1) - j) {
+					pattern += sign + " ";
+
+				} else {
+					pattern += " " + " ";
+				}
+			}
+			pattern += "\n";
+		}
 
 //		System.out.print(pattern); // for debug only
 		return pattern;
